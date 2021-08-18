7078920cbd10455c5e42e0a1b97af1cf0e707e79
Dekpiano <dekpiano4815@gmail.com>
2021-08-18 23:43:58 +0700

    18/08/64  เพิ่มแบนเนอร์วันวิทย์ 64

---
+++ application/views/user/popupSpecial/skj-640818-SciDay.php
@@ -0,0 +1,72 @@
+<style>
+/* // Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+    .skjSciDay .banner{
+        padding-top: 100px;
+    }
+    .skjSciDay h1{
+        font-size: 2rem;
+    }
+    .skjSciDay h2{
+        font-size: 1.5rem;
+    }
+    .skjSciDay h3{
+        font-size: 1.3rem;
+    }
+}
+
+/* // Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {}
+
+/* // Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {
+    .skjSciDay h1 {
+        font-size: 4rem;
+    }
+
+    .skjSciDay h2 {
+        font-size: 3rem;
+    }
+}
+
+/* // Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {}
+
+/* // Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+    .skjSciDay {}
+
+    .skjSciDay h1 {
+        font-size: 3rem;
+    }
+
+    .skjSciDay h2 {
+        font-size: 2rem;
+    }
+
+}
+</style>
+
+<div class="skjSciDay" style=" background-image: linear-gradient(180deg, #5552fd, #2576d0)">
+    <div class="container ">
+        <div class="row vh-100"></div>
+        <div class="col-md-12 col-lg-4 align-self-md-center banner">
+                <img src="<?=base_url('uploads/banner/64SciDay/111.png');?>" alt="" class="img-fluid">
+            </div>
+        <div class="col-md-12 col-lg-4 align-self-md-center Vlogger-setion1">
+                <div class="text-center">
+                    <h2 class="text-white"> สัปดาห์วิทยาศาสตร์ 2564</h2>
+                    <h1 class="text-white"> ขอเชิญชมนิทรรศการ </h1>
+                    <a href="https://sites.google.com/skj.ac.th/sci-tech/" target="_blank"
+                        class="btn btn-primary">ชมนิทรรศการ</a>
+                </div>
+            </div>
+        <div class="col-md-12 col-lg-4 align-self-md-center banner">
+                <img src="<?=base_url('uploads/banner/64SciDay/222.png');?>" alt="" class="img-fluid">
+            </div>
+           
+           
+
+        </div>
+    </div>
+</div>
