216926e6103b9a0467148294601e48449b647d22
dekpiano <dekpiano4815@gmail.com>
2021-10-31 21:40:16 -0700

    31/10/64 แก้ไขแบนเนอร์ ตารางเรียน

--- application/views/user/popupSpecial/skj-640603-ClassSchedule.php
+++ application/views/user/popupSpecial/skj-640603-ClassSchedule.php
@@ -1,58 +1,137 @@
 <style>
 /* // Extra small devices (portrait phones, less than 576px) */
 @media (max-width: 575.98px) {
-    .skjStudentList .banner{
+    .skjClassSchedule .banner{
         padding-top: 0px;
     }
-    .skjStudentList .setion-text{
+    .skjClassSchedule .setion1{
         padding-top: 100px;
     }
-    .skjStudentList h1{
+    .skjClassSchedule h1{
         font-size: 2rem;
     }
-    .skjStudentList h2{
+    .skjClassSchedule h2{
         font-size: 1.5rem;
     }
-    .skjStudentList h3{
+    .skjClassSchedule h3{
         font-size: 1.3rem;
     }
 }
 
 /* // Small devices (landscape phones, 576px and up) */
-@media (min-width: 576px) and (max-width: 767.98px) {}
+@media (min-width: 576px) and (max-width: 767.98px) {
+    .skjClassSchedule img {
+        max-width: 45%;
+        position: absolute;
+        margin: -150px 0px;
+    }
+
+    .skjClassSchedule h1 {
+        font-size: 3rem;
+        color: #fbd54a;
+        ;
+    }
+
+    .skjClassSchedule h2 {
+        font-size: 1.5rem;
+        color: #f8f9fa;
+    }
+
+    .skjClassSchedule h3 {
+        font-size: 1rem;
+        color: #f8f9fa;
+    }
+
+    .skjClassSchedule .setion1 {
+        margin-top: 40px;
+    }
+}
 
 /* // Medium devices (tablets, 768px and up) */
-@media (min-width: 768px) and (max-width: 991.98px) {}
+@media (min-width: 768px) and (max-width: 991.98px) {
+   
+    .skjClassSchedule h1 {
+        font-size: 4rem;
+        color: #fbd54a;
+        ;
+    }
+
+    .skjClassSchedule h2 {
+        font-size: 2rem;
+        color: #f8f9fa;
+    }
+
+    .skjClassSchedule h3 {
+        font-size: 1.5rem;
+        color: #f8f9fa;
+    }
+
+    .skjClassSchedule .setion1 {
+        margin-top: 120px;
+        text-align: center;
+    }
+
+    .skjClassSchedule img {
+        max-width: 100%;
+    margin-top: 95px;
+    }
+}
 
 /* // Large devices (desktops, 992px and up) */
-@media (min-width: 992px) and (max-width: 1199.98px) {}
+@media (min-width: 992px) and (max-width: 1199.98px) {
+    
+
+    .skjClassSchedule h1 {
+        font-size: 4rem;
+        color: #fbd54a;
+    }
+
+    .skjClassSchedule h2 {
+        font-size: 2rem;
+        color: #f8f9fa;
+    }
+
+    .skjClassSchedule h3 {
+        font-size: 1.3rem;
+        color: #f8f9fa;
+    }
+
+    .skjClassSchedule .setion1 {
+
+        text-align: center;
+    }
+}
+
 
 /* // Extra large devices (large desktops, 1200px and up) */
 @media (min-width: 1200px) {
-    .skjStudentList {}
+    .skjClassSchedule {}
 
-    .skjStudentList h1 {
+    .skjClassSchedule h1 {
         font-size: 5rem;
     }
 
 }
 </style>
 
-<div class="skjStudentList" style=" background-image: linear-gradient(180deg, #52a0fd, #e8f2fd);">
+<div class="skjClassSchedule" style=" background-image: linear-gradient(180deg, #293649, #293649);">
     <div class="container ">
         <div class="row vh-100">
-        <div class="col-md-7 align-self-md-center setion-text">
-                <div class="">
-                    <h1> ตารางเรียนออนไลน์</h1>
-                    <h2> ให้นักเรียนดูตารางเรียนปรับปรุงโควิด 19 เพื่อสะดวกในการเข้าเรียนของนักเรียน ใช้เรียนตั้งแต่วันที่ 2 สิงหาคม 2564 เป็นต้นไปจนจบภาคเรียนที่ 1/2564  หรือ จนกว่าสถานการณ์ COVID 19 จะคลี่คลาย </h2>
+        <div class="col-md-12 col-lg-8 col-xl-7 align-self-md-center setion1  animate__animated animate__fadeInLeft animate__delay-1s">
+                <div >
+                    <h1 class="text-white"> ตารางเรียนออนไลน์</h1>
+                    <h2 class="text-white"> 
+                        ภาคเรียนที่ 2 ปีการศึกษา 2564 <br>
+                        ใช้เรียนตั้งแต่ 1 พฤศจิกายน 2564
+                    </h2>
                     <a href="http://academic.skj.ac.th/ClassSchedule" target="_blank"
                         class="btn btn-primary mb-2">ค้นหาตารางเรียน</a>
                         <a href="https://academic.skj.ac.th/LearningOnline" target="_blank"
                         class="btn btn-primary mb-2">ห้องเรียนออนไลน์</a>
                 </div>
             </div>
-        <div class="col-md-5 align-self-md-center banner">
-                <img src="<?=base_url('uploads/banner/64ClassSchedule/banner.svg');?>" alt="" class="img-fluid">
+        <div class="col-md-12 col-lg-4 col-xl-5 align-self-md-center banner animate__animated animate__fadeInRight animate__delay-1s">
+                <img src="<?=base_url('uploads/banner/64ClassSchedule/logo.svg');?>" alt="" class="img-fluid">
             </div>
            
            
--- application/views/user/popupSpecial/skj-slide.php
+++ application/views/user/popupSpecial/skj-slide.php
@@ -34,15 +34,17 @@
         <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
     </ol>
     <div class="carousel-inner">
-
-        <!-- <div class="carousel-item ">
-            <?php $this->load->view('user/popupSpecial/skj-641019-Vaccine2.php'); ?>
-        </div> -->
-        
-        <div class="carousel-item active">
-            <?php $this->load->view('user/popupSpecial/skj-640629-Online.php'); ?>
+    <div class="carousel-item active">
+            <?php $this->load->view('user/popupSpecial/skj-640603-ClassSchedule.php'); ?>
         </div>
         <!-- <div class="carousel-item ">
+            <?php $this->load->view('user/popupSpecial/skj-641019-Vaccine2.php'); ?>
+        </div>
+        
+        <div class="carousel-item ">
+            <?php $this->load->view('user/popupSpecial/skj-640629-Online.php'); ?>
+        </div>
+        <div class="carousel-item ">
             <?php $this->load->view('user/popupSpecial/skj-640610-Classroom.php'); ?>
         </div>
         <div class="carousel-item ">
