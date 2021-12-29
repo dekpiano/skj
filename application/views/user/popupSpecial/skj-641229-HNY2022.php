177caec07f530eaeaa6880473c46d4489c6cce2a
dekpiano <dekpiano4815@gmail.com>
2021-12-29 14:04:37 +0700

    29/12/64 เพื่มหน้าาตอนรับ ปีใหม่ 64

---
+++ application/views/user/popupSpecial/skj-641229-HNY2022.php
@@ -0,0 +1,403 @@
+<link href="https://fonts.googleapis.com/css2?family=Imperial+Script&display=swap" rel="stylesheet">
+<style>
+/* // Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+    .skjHNY2022 h1 {
+        font-size: 2.5rem;
+        line-height: 65px;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h2 {
+        font-size: 1.3rem;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h3 {
+        font-size: 1.3rem;
+    }
+
+	.skjHNY2022 .setion-left {
+		padding-top: 100px;
+    margin-bottom: -165px;
+    }
+	.skjHNY2022 .setion-left img {
+        width: 270px;
+    }
+
+	.skjHNY2022 .img-buttom img {
+        position: absolute;
+        width: 400px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 0px;
+    }
+}
+
+/* // Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {
+	.skjHNY2022 h1 {
+        font-size: 2.5rem;
+        line-height: 65px;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h2 {
+        font-size: 1.3rem;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h3 {
+        font-size: 1.3rem;
+    }
+
+	.skjHNY2022 .setion-left {
+		padding-top: 100px;
+    margin-bottom: -165px;
+    }
+	.skjHNY2022 .setion-left img {
+        width: 270px;
+    }
+
+	.skjHNY2022 .img-buttom img {
+        position: absolute;
+        width: 400px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 0px;
+    }
+}
+
+/* // Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {
+
+    .skjHNY2022 h1 {
+        font-size: 5rem;
+        color: #fbd54a;
+        line-height: 92px;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h2 {
+        font-size: 1.9rem;
+        color: #f8f9fa;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h3 {
+        font-size: 1.5rem;
+        color: #f8f9fa;
+    }
+
+    .skjHNY2022 .setion1 {
+        margin-top: -350px;
+        text-align: center;
+    }
+
+    .skjHNY2022 img {
+        max-width: 100%;
+        margin-top: 95px;
+    }
+
+    .skjHNY2022 .setion-right {
+        margin-bottom: 215px;
+    }
+	.skjHNY2022 .setion-left img {
+        width: 500px;
+    }
+	.skjHNY2022 .img-buttom img {
+        position: absolute;
+        width: 1200px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 0px;
+    }
+}
+
+/* // Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {
+	.skjHNY2022 h1 {
+        font-size: 4.5rem;
+        line-height: 65px;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h2 {
+        font-size: 1.3rem;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h3 {
+        font-size: 1.3rem;
+    }
+
+	.skjHNY2022 .setion-left {
+		
+    }
+	.skjHNY2022 .setion-left img {
+        width: 500px;
+    }
+
+	.skjHNY2022 .img-buttom img {
+        position: absolute;
+        width: 980px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 0px;
+    }
+}
+
+
+/* // Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+    .skjHNY2022 {}
+
+    .skjHNY2022 h1 {
+        font-size: 5rem;
+        line-height: 100px;
+		text-shadow: 2px 2px #5a5a5a;
+    }
+
+    .skjHNY2022 h2 {
+        font-size: 1.5rem;
+		text-shadow: 1px 1px #000000;
+    }
+
+    .skjHNY2022 .setion-left {}
+
+    .skjHNY2022 .setion-right {
+        margin-bottom: 130px;
+    }
+
+    .skjHNY2022 .setion-left img {
+        width: 500px;
+    }
+
+    .skjHNY2022 .img-buttom {
+        margin-bottom: -40px;
+    }
+	.skjHNY2022 .img-buttom img {
+        position: absolute;
+        width: 1200px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 0px;
+    }
+    
+}
+
+
+.snowflake {
+    color: #fff;
+    font-size: 1em;
+    font-family: Arial;
+    text-shadow: 0 0 1px #000;
+}
+
+@-webkit-keyframes snowflakes-fall {
+    0% {
+        top: -10%
+    }
+
+    100% {
+        top: 100%
+    }
+}
+
+@-webkit-keyframes snowflakes-shake {
+    0% {
+        -webkit-transform: translateX(0px);
+        transform: translateX(0px)
+    }
+
+    50% {
+        -webkit-transform: translateX(80px);
+        transform: translateX(80px)
+    }
+
+    100% {
+        -webkit-transform: translateX(0px);
+        transform: translateX(0px)
+    }
+}
+
+@keyframes snowflakes-fall {
+    0% {
+        top: -10%
+    }
+
+    100% {
+        top: 100%
+    }
+}
+
+@keyframes snowflakes-shake {
+    0% {
+        transform: translateX(0px)
+    }
+
+    50% {
+        transform: translateX(80px)
+    }
+
+    100% {
+        transform: translateX(0px)
+    }
+}
+
+.snowflake {
+    position: fixed;
+    top: -10%;
+    z-index: 9999;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    cursor: default;
+    -webkit-animation-name: snowflakes-fall, snowflakes-shake;
+    -webkit-animation-duration: 10s, 3s;
+    -webkit-animation-timing-function: linear, ease-in-out;
+    -webkit-animation-iteration-count: infinite, infinite;
+    -webkit-animation-play-state: running, running;
+    animation-name: snowflakes-fall, snowflakes-shake;
+    animation-duration: 10s, 3s;
+    animation-timing-function: linear, ease-in-out;
+    animation-iteration-count: infinite, infinite;
+    animation-play-state: running, running
+}
+
+.snowflake:nth-of-type(0) {
+    left: 1%;
+    -webkit-animation-delay: 0s, 0s;
+    animation-delay: 0s, 0s
+}
+
+.snowflake:nth-of-type(1) {
+    left: 10%;
+    -webkit-animation-delay: 1s, 1s;
+    animation-delay: 1s, 1s
+}
+
+.snowflake:nth-of-type(2) {
+    left: 20%;
+    -webkit-animation-delay: 6s, .5s;
+    animation-delay: 6s, .5s
+}
+
+.snowflake:nth-of-type(3) {
+    left: 30%;
+    -webkit-animation-delay: 4s, 2s;
+    animation-delay: 4s, 2s
+}
+
+.snowflake:nth-of-type(4) {
+    left: 40%;
+    -webkit-animation-delay: 2s, 2s;
+    animation-delay: 2s, 2s
+}
+
+.snowflake:nth-of-type(5) {
+    left: 50%;
+    -webkit-animation-delay: 8s, 3s;
+    animation-delay: 8s, 3s
+}
+
+.snowflake:nth-of-type(6) {
+    left: 60%;
+    -webkit-animation-delay: 6s, 2s;
+    animation-delay: 6s, 2s
+}
+
+.snowflake:nth-of-type(7) {
+    left: 70%;
+    -webkit-animation-delay: 2.5s, 1s;
+    animation-delay: 2.5s, 1s
+}
+
+.snowflake:nth-of-type(8) {
+    left: 80%;
+    -webkit-animation-delay: 1s, 0s;
+    animation-delay: 1s, 0s
+}
+
+.snowflake:nth-of-type(9) {
+    left: 90%;
+    -webkit-animation-delay: 3s, 1.5s;
+    animation-delay: 3s, 1.5s
+}
+
+/* Demo Purpose Only*/
+.demo {
+    font-family: 'Raleway', sans-serif;
+
+    display: block;
+    margin: 0 auto;
+    padding: 15px 0;
+    text-align: center;
+}
+
+.demo a {
+    font-family: 'Raleway', sans-serif;
+    color: #000;
+}
+</style>
+
+<div class="snowflake">
+    ❅
+</div>
+<div class="snowflake h3">
+    ❅
+</div>
+<div class="snowflake">
+    ❆
+</div>
+<div class="snowflake">
+    ❄
+</div>
+<div class="snowflake">
+    ❅
+</div>
+<div class="snowflake h3">
+    ❆
+</div>
+<div class="snowflake">
+    ❄
+</div>
+<div class="snowflake">
+    ❅
+</div>
+<div class="snowflake h3">
+    ❆
+</div>
+<div class="snowflake">
+    ❄
+</div>
+
+<div class="skjHNY2022" style="background-image: linear-gradient(180deg, #6dc0ff, #2576d0);background-image:url(uploads/banner/64HNY2022/bg.svg);background-size: cover;">
+    <div class="container ">
+        <div class="row vh-100">
+            <div
+                class="col-md-12 col-lg-6 col-xl-6 align-self-md-center setion-left  animate__animated animate__bounceIn animate__delay-1s">
+                <div class="text-center">
+                    <img loading="lazy" src="<?=base_url('uploads/banner/64HNY2022/all.png');?>" alt="">
+
+                </div>
+            </div>
+            <div
+                class="col-md-12 col-lg-6 col-xl-6 align-self-md-center setion-right animate__animated animate__bounceIn animate__delay-1s">
+                <div class="text-center">
+                    <h1 class="text-white" style="font-family: 'Pattaya', cursive;"> สวัสดีปีใหม่ 2565</h1>
+                    <h2 class="text-white" style="">
+                        ในวารดีถีขึ้นปีใหม่ ขออำนาจสิ่งศักดิ์สิทธิ์ทั้งหลายในสากลโลก จงดลบันดาลให้ท่าน
+                        ประสบแต่ความสุขเกษมสำราญและสัมฤทธิผลในสิ่งอันพึงปราถนาทุกประการ
+                    </h2>
+                </div>
+            </div>
+            <div class="img-buttom ">
+                <img loading="lazy" src="<?=base_url('uploads/banner/64HNY2022/ครู.png');?>" alt="">
+            </div>
+        </div>
+
+
+    </div>
+</div>
